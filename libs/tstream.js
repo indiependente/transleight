d358e655712a4fd40566d3f07cc00110b54d2321
indiependente <rockerg991@gmail.com>
2017-02-13 09:53:35 +0000

    gzip and deflate compression handling added

--- libs/tstream.js
+++ libs/tstream.js
@@ -4,15 +4,17 @@ var Stream      = 	require('stream');
 var chunknlines = 	require('chunknlines');
 var urlencode   =	require('urlencode');
 var through		=	require('through');
-const token = require('google-translate-token'); // recent changes in Google Translate request params need this token
-// var zlib        =	require('zlib');
+const token 	= 	require('google-translate-token'); // recent changes in Google Translate request params need this token
+// var oppressor   =	require('oppressor');
+var zlib		=	require('zlib');
+
 
 var tstream = function(lang){
 
 	var encoder = through(
 					function write(buf){this.queue(urlencode(buf));},
 					function end(buf){if(arguments.length) write(buf);this.queue(null);}
-					);
+				);
 
 	var target = lang;
 	var translator = new Stream;
@@ -31,13 +33,14 @@ var tstream = function(lang){
 							headers : 	{
 										'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 										'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0',
-										// 'accept-encoding' : 'plain',
+										'Accept-Encoding' : 'gzip, deflate'
 										// 'Cookie' : 'NID=92=EQcfF_xg9OVwdR1eKIHRxZYWzd8GC6FnzSh4ku6EOLySSYe7QhPD1_fiBkL4ugqZmwfSh2YNe08JJ0uK8pNNal5GBoyiMQKgr3mw5525_6VkKfHsUx3SF9raE9rSCl7kd_RqttiQWT-JMMLfhtXd44LyYZM9ifzdJDiOTFs1azeUsSUVSqN8ZkI; SID=HQTBhlJSBvVQKqVT6XTuCI0sly32d5BPBPyufoDn8Pvx2B2-fNGOXHf6Ps23qIRyX0X1Yw.; HSID=A9x9FNynn2CwOn1Hr; SSID=All2pUWWLcnb5Crxv; APISID=HF41EQ37Vr7dkyLo/A2Obvon_0c82BDGG7; SAPISID=lLuEgzcdks9CT1zr/Am1O3o2AdytfZvnb8; CONSENT=YES+IT.it+20150628-20-0; GMAIL_RTT=73',
-										// // 'Connection' : 'keep-alive',
+										// 'Connection' : 'keep-alive',
 										// 'Host' : 'translate.google.com',
 										// 'Upgrade-Insecure-Requests' : '1'
 										},
-							method 	: "GET"
+							encoding: null,
+							method 	: 'GET'
 					};
 		
 					options.url = options.url.replace(/\$\$\$/g, target);
@@ -48,8 +51,19 @@ var tstream = function(lang){
 					var res_stream = request(options, function(err, res, body){
 						// console.log(res.statusCode + ' ' + res.statusMessage)
 						if (!err && res.statusCode == 200) {
-							// console.log(body.toString());
-							translator.emit('data', body);
+							var encoding = res.headers['content-encoding'];
+							if (encoding && (encoding.indexOf('gzip') >= 0 || encoding.indexOf('deflate') >= 0)) {
+								const buffer = Buffer.from(body);
+								zlib.unzip(buffer, function (err, unzippedbuffer) {
+									if (err)
+										throw err;
+									translator.emit('data', unzippedbuffer);
+								});
+							}
+							else {
+								console.log('plain');
+								translator.emit('data', body);
+							}
 							translator.pendings--;
 						}
 					});
@@ -71,7 +85,8 @@ var tstream = function(lang){
 					function end(buf){if(arguments.length) write(buf);this.queue(null);}
 					);
 
-	return combine(chunknlines(), encoder, translator, decoder);//, zlib.createGunzip());
+	return combine(chunknlines(), encoder, translator, decoder);
+	// return combine(chunknlines(), encoder, translator, zlib.createGunzip());
 
 };
 
